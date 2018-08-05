87aea1a01496a13ca2decb81f8db902d3f59c0a9
ciyang <jason21131@gmail.com>
2018-08-05 23:46:15 +0800

    Get the all articles of each page of specific board, parse articles by start page and pages.

--- crawler.py
+++ crawler.py
@@ -45,9 +45,54 @@ def get_latest_page(boardName):
 
 	return index
 
+def parse_articles(start, page, board, timeout=3):
+	# 給定page數量從最新的頁面開始爬取
+	# 爬的是個版頁面底下的所有文章標題
+	print('Starting '+ board + ' ...')
+
+	if(start == "Null"):
+		return
+	print(int(start), page)
+
+	for i in range(int(start), int(start)-int(page), -1):
+		index = i
+		print('Processing index:', str(index))
+		isTimeoutError = True			# handdle timeout exception
+		while isTimeoutError:
+			try:
+				resp = requests.get(
+					url = PTT_URL + '/bbs/' + board + '/index' + str(index) + '.html',
+					cookies={'over18': '1'}, timeout=3
+				)
+				isTimeoutError = False
+			except requests.exceptions.Timeout:
+				# Timeout exception
+				
+				print("retry")
+		if resp.status_code != 200:
+			print('invalid url:', resp.url)
+			continue
+		soup = BeautifulSoup(resp.text, 'html.parser')
+		divs = soup.find_all("div", "r-ent")
+
+		for div in divs:
+			href = div.find('a')['href']
+			link = PTT_URL + href
+			article_id = re.sub('\.html', '', href.split('/')[-1])
+			#ptt_obj = parse(link, article_id, board)
+			print('article id: ', article_id)
+
+		time.sleep(0.1)
+
+	print("Done wih all pages")
+
+
 if __name__ == "__main__":
 	# Enter board name you want to crawler
 	board_input_Name = str(sys.argv[1])
+	start = get_latest_page(board_input_Name)
+	page = 1
 
-	print("Index numbers: ", get_latest_page(board_input_Name))
+	print("Index numbers: ", start)
+	parse_articles(start, page, board_input_Name)
 
