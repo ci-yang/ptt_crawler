ac2c8c8eb3139157beb8e478cfb92358bbd72b54
ciyang <jason21131@gmail.com>
2018-08-06 02:08:08 +0800

    Fix the indents and handle error exception, add outputting the JSON file, version 1

--- crawler.py
+++ crawler.py
@@ -58,6 +58,7 @@ def get_latest_page(boardName):
 def parse_articles(start, page, board, timeout=3):
 	# 給定page數量從最新的頁面開始爬取
 	# 爬的是個版頁面底下的所有文章標題
+	result = []
 	print('Starting '+ board + ' ...')
 
 	if(start == "Null"):
@@ -89,16 +90,21 @@ def parse_articles(start, page, board, timeout=3):
 		divs = soup.find_all("div", "r-ent")
 
 		for div in divs:
-			href = div.find('a')['href']
-			link = PTT_URL + href
-			article_id = re.sub('\.html', '', href.split('/')[-1])
-			ptt_obj = parse(link, article_id, board)
-			print('article id: ', article_id)
-			print(ptt_obj)
+			try:
+				href = div.find('a')['href']
+				link = PTT_URL + href
+				article_id = re.sub('\.html', '', href.split('/')[-1])
+				ptt_obj = parse(link, article_id, board)
+				print('article id: ', article_id)
+				#print(ptt_obj)
+				result.append(ptt_obj)
+			except TypeError:
+				print("Article maybe deleted!!")
 
 		time.sleep(0.1)
 
 	print("Done wih all pages")
+	return result
 
 
 def parse(link, article_id, board, timeout=3):
@@ -139,79 +145,79 @@ def parse(link, article_id, board, timeout=3):
 		for meta in main_content.select('div.article-metaline-right'):
 			meta.extract()
 
-		# 做完之後剩下內文跟推文
-		# 將內文所有結構存起來
-		content_html = main_content
+	# 做完之後剩下內文跟推文
+	# 將內文所有結構存起來
+	content_html = main_content
 
-		# remove and keep push nodes
-		pushes = main_content.find_all('div', class_='push')
+	# remove and keep push nodes
+	pushes = main_content.find_all('div', class_='push')
 
-		#IP
-		try:
-			ip = main_content.find(text=re.compile(u'※ 發信站:'))
-			ip = re.search('[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*', ip).group()
-		except:
-			ip = "None"
+	#IP
+	try:
+		ip = main_content.find(text=re.compile(u'※ 發信站:'))
+		ip = re.search('[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*', ip).group()
+	except:
+		ip = "None"
 
-		#引述的文章作者
-		ref_text = main_content.find(text=re.compile(u'※ 引述'))
+	#引述的文章作者
+	ref_text = main_content.find(text=re.compile(u'※ 引述'))
 
-		if(ref_text is not None):
-			left = ref_text.find('\u300a')
-			right = ref_text.find('\u300b')
+	if(ref_text is not None):
+		left = ref_text.find('\u300a')
+		right = ref_text.find('\u300b')
 
-			if(left and right):
-				isReference = 1
-				ref_author_name = ref_text[left+1:right]
-				ref_author_ID = removeSybol(ref_author_name)
-			else:
-				ref_author_name = ''
-				ref_author_ID = ''
+		if(left and right):
+			isReference = 1
+			ref_author_name = ref_text[left+1:right]
+			ref_author_ID = removeSybol(ref_author_name)
 		else:
-			ref_text = ""
-			isReference = 0
+			ref_author_name = ''
+			ref_author_ID = ''
+	else:
+		ref_text = ""
+		isReference = 0
 
-		# push messages
-		p, b, n = 0, 0, 0		# push tag, boo tag, arraw tag
-		messages = []
-		for push in pushes:
-			if not push.find('span', 'push-tag'):
-				continue
-			push_tag = push.find('span', 'push-tag').string.strip(' \t\n\r')
-			push_userid = push.find('span', 'push-userid').string.strip(' \t\n\r')
+	# push messages
+	p, b, n = 0, 0, 0		# push tag, boo tag, arraw tag
+	messages = []
+	for push in pushes:
+		if not push.find('span', 'push-tag'):
+			continue
+		push_tag = push.find('span', 'push-tag').string.strip(' \t\n\r')
+		push_userid = push.find('span', 'push-userid').string.strip(' \t\n\r')
 
-			# if find is None: find().strings -> list -> ' '.join; else the current way
-			push_content = push.find('span', 'push-content').strings
-			push_content = ' '.join(push_content)[1:].strip(' \t\n\r')  # remove ':'
-			push_ipdatetime = push.find('span', 'push-ipdatetime').string.strip(' \t\n\r')
-			messages.append( {'push_tag': push_tag, 'push_userid': push_userid, 'push_content': push_content, 'push_ipdatetime': push_ipdatetime} )
-			if push_tag == u'推':
-				p += 1
-			elif push_tag == u'噓':
-				b += 1
-			else:
-				n += 1
+		# if find is None: find().strings -> list -> ' '.join; else the current way
+		push_content = push.find('span', 'push-content').strings
+		push_content = ' '.join(push_content)[1:].strip(' \t\n\r')  # remove ':'
+		push_ipdatetime = push.find('span', 'push-ipdatetime').string.strip(' \t\n\r')
+		messages.append( {'push_tag': push_tag, 'push_userid': push_userid, 'push_content': push_content, 'push_ipdatetime': push_ipdatetime} )
+		if push_tag == u'推':
+			p += 1
+		elif push_tag == u'噓':
+			b += 1
+		else:
+			n += 1
 
-		data = {
-			"author": author,                       #作者
-			"authorID": removeSybol(author),		#去除括弧的作者ID
-			"title": title,                         #標題
-			"date": date,                           #時間
-			"ip": ip,                               #IP
-			"article_url": link,                    #文章地址
-			"content_html": content_html,           #原始內文html結構
-			"isReference": isReference,             #使否引述文章
-			"ref_text": ref_text,                   #原始引述字串
-			"ref_author_ID": ref_author_ID,         #引述的作者ＩＤ
-			"ref_author_name": ref_author_name,     #引述的作者ID + 暱稱
-			"messages": messages,                   #回文的內容
-			"total_messages": p+b+n,                #回文總數
-			"push_amount": p,                       #推文數
-			"boo_amount": b,                        #噓文數
-			"arrow_amount": n                       #箭頭數
-		}
-		
-		return data
+	data = {
+		"author": author,                       #作者
+		"authorID": removeSybol(author),		#去除括弧的作者ID
+		"title": title,                         #標題
+		"date": date,                           #時間
+		"ip": ip,                               #IP
+		"article_url": link,                    #文章地址
+		"content_html": str(content_html),           #原始內文html結構
+		"isReference": isReference,             #使否引述文章
+		"ref_text": ref_text,                   #原始引述字串
+		"ref_author_ID": ref_author_ID,         #引述的作者ＩＤ
+		"ref_author_name": ref_author_name,     #引述的作者ID + 暱稱
+		"messages": messages,                   #回文的內容
+		"total_messages": p+b+n,                #回文總數
+		"push_amount": p,                       #推文數
+		"boo_amount": b,                        #噓文數
+		"arrow_amount": n                       #箭頭數
+	}
+	
+	return data
 
 
 if __name__ == "__main__":
@@ -220,6 +226,14 @@ if __name__ == "__main__":
 	start = get_latest_page(board_input_Name)
 	page = 1
 
-	print("Index numbers: ", start)
-	parse_articles(start, page, board_input_Name)
+	if(start is not "Null"):
+		print("Index numbers: ", int(start)-1)
+		data = parse_articles(int(start)-1, page, board_input_Name)
+	else:
+		data = {
+			"status": "error"
+		}
+
+	with open(str(int(start)-page) + '-' + str(int(start)-1) + '.json', 'w') as outfile:
+		json.dump(data, outfile)
 
